f76548d8c65ec33455f53360dc522672db02e67e
Michal-Z9 <michalzar.2009@gmail.com>
2026-02-08 11:52:34 +0100

    Create Untitled-1.cpp

---
+++ Untitled-1.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b;
+    cout<<"enter two numbers: ";
+    cin>>a>>b;
+    cout<<"the sum of "<<a<<" and "<<b<<" id: "<<a+b;
+
+
+}
+
+
